11838bba9e215a6623c716348ca5e8a745b355b7
Salomao <salomao.machado.75@gmail.com>
2023-06-14 17:13:39 -0300

    <feat> Integração API : criação de arquivo service para consulta na api. integração da api viacep para o campo cep.

--- forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CepService } from '../services/consulta/cep.service';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -8,10 +10,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
+  
+  constructor(
+    private router: Router,
+    private cepService: CepService
+    ) { }
+    
+    
   ngOnInit(): void {
+
+  }
+
+  consultaCEP(ev: any){
+    const cep = ev.target.value;
+    return this.cepService.getConsultaCep(cep).subscribe(resultado => console.log(resultado));
   }
 
   cadastrar(form: NgForm){
@@ -22,4 +34,5 @@ export class CadastroComponent implements OnInit {
     }
     console.log(form.controls);
   }
+
 }
